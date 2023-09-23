bdc525a47dde4337905f80baf6ac93fb9223a08c
Wainaina <ynainajoseph@gmail.com>
2023-09-23 22:22:50 +0300

    challenge done

---
+++ CsharpProjects/CodeChallegeOne/Program.cs
@@ -0,0 +1,20 @@
+﻿string? userInput = "";
+int inputToInt = 0;
+bool isValidInput = true;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do {
+    userInput = Console.ReadLine();
+
+    isValidInput = int.TryParse(userInput, out inputToInt);
+    if(!isValidInput)
+         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    else if(!(inputToInt >=5 && inputToInt <= 10))
+        Console.WriteLine($"You entered {inputToInt}. Please enter a number between 5 and 10.");
+    else
+        break;
+
+} while(true);
+
+Console.WriteLine($"Your input value ({inputToInt}) has been accepted.");
---
+++ CsharpProjects/ValidateStringInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+var roles = new List<string> {"administrator", "manager", "user"};
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+bool isInValid = false;
+
+do{
+    string? userInput = Console.ReadLine();
+    
+    if(userInput != null)
+    {
+        userInput = userInput.Trim();
+        userInput = userInput.ToLower();
+
+        isInValid = roles.Contains(userInput);
+
+        string message = roles.Contains(userInput) ?      
+            $"Your input value ({userInput}) has been accepted."   
+            :
+            $"The role name that you entered, \"{userInput}\" is not valid. Enter your role name (Administrator, Manager, or User)"
+            ;
+        Console.WriteLine(message);
+    } 
+
+} while(!isInValid);
