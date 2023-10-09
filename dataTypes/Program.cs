86070f2b2a0454765e9de7f87080e60ef8853446
Wainaina <ynainajoseph@gmail.com>
2023-10-09 21:28:05 +0300

    type conversions

---
+++ dataTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+
+// second.length();
+
+int name = 123;
+
+Console.WriteLine(Convert.ToByte(name));
