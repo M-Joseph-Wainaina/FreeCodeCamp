ee16583ac2f521f3d789884f3a246ca9b8e6278c
Wainaina <ynainajoseph@gmail.com>
2023-11-08 19:07:54 +0300

    contoso petting zoo

--- ContosoPettingZoo/Program.cs
+++ ContosoPettingZoo/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+RandomizeAnimals();
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    int a, b;
+    string temp;
+
+    for(int i = 0; i < pettingZoo.Length; i++)
+    {
+        a = random.Next(pettingZoo.Length);
+        b = random.Next(pettingZoo.Length);
+
+        temp = pettingZoo[a];
+        pettingZoo[a] = pettingZoo[b];
+        pettingZoo[b] = temp;
+
+    }
+    foreach(var animal in pettingZoo)
+    {
+        Console.Write(animal + " ,");
+    }
+}
+
+// AssignGroup();
--- ContosoPettingZoo/README.md
+++ ContosoPettingZoo/README.md
@@ -9,4 +9,4 @@
     - Print the school name
     - Print the animal groups
 
-[Click Me](https://learn.microsoft.com/en-gb/training/modules/guided-project-visit-petting-zoo/3-exercise-plan-use-methods)
+[Click Me](https://learn.microsoft.com/en-gb/training/modules/guided-project-visit-petting-zoo/3-exercise-plan-use-methodsc)
