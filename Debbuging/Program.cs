ed3eb0052e5fba00a618e6af431ea9b2bd78f1f0
Wainaina <ynainajoseph@gmail.com>
2023-11-14 22:12:42 +0300

    playing around with debugger

---
+++ Debbuging/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length;
+
+sayHello();
+try {
+    Console.WriteLine("The final name is: " + students[studentCount]);
+} catch(IndexOutOfRangeException x) {
+    Console.WriteLine($"{x.Message}");
+}
+void sayHello()
+{
+    int a ;
+    int b; 
+    a = 0; 
+    Console.WriteLine("Hello ");
+}
