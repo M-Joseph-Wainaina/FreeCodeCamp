57ff8e9b9b87719e118f5f275148855089640bb2
Wainaina <ynainajoseph@gmail.com>
2023-11-15 18:58:46 +0300

    debugging tools intro in c#

--- Debbuging/Program.cs
+++ Debbuging/Program.cs
@@ -2,16 +2,9 @@
 
 int studentCount = students.Length;
 
-sayHello();
+
 try {
     Console.WriteLine("The final name is: " + students[studentCount]);
 } catch(IndexOutOfRangeException x) {
     Console.WriteLine($"{x.Message}");
-}
-void sayHello()
-{
-    int a ;
-    int b; 
-    a = 0; 
-    Console.WriteLine("Hello ");
 }
