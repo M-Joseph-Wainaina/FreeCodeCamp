1bc335a57916353180683a14fab78c41cf2b9fbf
Joseph Wainaina <ynainajoseph@gmail.com>
2023-11-28 21:34:39 +0300

    handliing specific types of exceptions

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,14 +1,33 @@
-﻿internal class Program
+﻿try
 {
-    private static void Main(string[] args)
-    {
-        double float1 = 3000.0;
-        double float2 = 0.0;
-        int number1 = 3000;
-        int number2 = 0;
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
 
-        Console.WriteLine(float1 / float2);
+Console.WriteLine("Exit program");
 
-        Console.WriteLine("Exit program");
-    }
+static void Process1()
+{
+   try  
+   {
+         WriteMessage();
+   }
+   catch(Exception ex)
+   {
+        Console.WriteLine($"Exception caught in process 1:   {ex.Message}");
+   }
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
 }
